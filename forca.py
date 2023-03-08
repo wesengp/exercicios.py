ce7a0e9489d8ee3336e08d13568dfd63c292991b
Wesley <Wesleyfonsecaengp@gmail.com>
2023-03-07 21:00:10 -0300

    Colocando a condição while no jogo

--- forca.py
+++ forca.py
@@ -1,6 +1,16 @@
 
-def jogar ():
-    print("**************************************")
-    print("BEM VINDO AO JOGO DA FORCA ")
-    print("**************************************")
-    print("Fim do Jogo")
+#def jogar ()
+print("**************************************")
+print("BEM VINDO AO JOGO DA FORCA ")
+print("**************************************")
+print("Vamos começar")
+    
+palavra_secreta = 'Goiaba'
+acertou = False
+enforcou = False
+
+while(not enforcou and not acertou):
+     print("Jogando....")
+
+    
+
